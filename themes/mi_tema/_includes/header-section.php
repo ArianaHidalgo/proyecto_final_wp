820ebfe8c6874510668e34bf167d448fa0825af7
Ariana <ariana.paz.b11@gmail.com>
2020-05-08 17:03:55 -0400

    pluggin

--- themes/mi_tema/_includes/header-section.php
+++ themes/mi_tema/_includes/header-section.php
@@ -3,6 +3,6 @@
             <p class="header_sub">HOTEL, VETERINARIA Y TIENDA ESPECIALIZADA EN FELINOS</p>
             <h1 class="header_tit">Mi Gato Azir</h1>
             <h3 class="header_tit2">Atencion a domicilio y envios a todo chile</h3>
-            <button class="header_button">URGENCIA</button>
+            <a class="whatsapp-web" href="https://web.whatsapp.com/send/?phone=56933217260&text=Necesito%20atención%20con%20urgencia%20"><button class="header_button">URGENCIA</button></a>
         </div>
 </header>
